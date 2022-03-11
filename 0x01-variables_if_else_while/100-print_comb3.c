915146518b9e82cd87250051b05cd70dccafe4d7
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-11 23:17:34 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all possible different combination of two
+ * digits but not 01 and 01 which is 18 on base ten.
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+
+			putchar('0' + d);
+			putchar('0' + c);
+
+			if (c + d != 18)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
