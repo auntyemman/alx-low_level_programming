d66218217cc13cb0e63abc920c85876e2bbbf7c0
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-11 22:18:52 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all digits of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 0; r <= 9; r++)
+{
+	printf("%d", r);
+}
+	printf("\n");
+	return (0);
+}
