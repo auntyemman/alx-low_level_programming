1bda88695cba64c7e4b9ea37db89917033e6177a
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-11 21:53:54 +0100

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: To display lower and UPPER case alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+			c++;
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+}
