cfc3599bfb6a300daf7f9ee9ec8a288d8754dafa
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-15 15:48:12 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * Main: The entry point of C program
+ * Description: The program prints _putchar
+ *
+ * Return: 0 means successs
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i;
+
+	for (i=0; i<9; i++)
+	{
+		_putchar("%c", c[i]);
+	}
+	return (0);
+}
