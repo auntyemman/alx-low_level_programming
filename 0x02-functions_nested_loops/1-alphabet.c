f7b885377022c0f2893c22669ca319aeee41aee2
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-15 23:01:51 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for Holberton School students.
+ * function: print_alphabet
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		putchar (alp);
+		alp++;
+
+	}
+	putchar ('\n');
+
+}
+int main (void)
+{
+	print_alphabet();
+	return (0);
+}
