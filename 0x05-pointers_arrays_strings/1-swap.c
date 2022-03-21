295586a55807bfcdc9fce90f046a7bb56a8dd58d
Adedayo Adepoju <1mole.coco@gmail.com>
2022-03-21 19:42:51 +0100

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,14 +3,12 @@
  * swap_int - swap variable values
  * @a: pointer 1
  * @b: pointer 2
- * @swap: variable storage
  * Return: void
 */
 
 void swap_int(int *a, int *b)
 {
-	int swap = *a;
-
-	*a = *b;
-	*b = swap;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
