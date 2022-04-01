09ef927cb911c063366b4e72fa1f1266823f88ef
Adedayo Adepoju <1mole.coco@gmail.com>
2022-04-01 20:02:03 +0100

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,7 @@
 
 int main(int argc, char const *argv[])
 {
-	/** since main function can only take zero or two parameters
-	 * we have to void the argv since we are not using it
-	 */
 	(void)argv;
-
 	printf("%i\n", argc - 1);
 	return (0);
 }
