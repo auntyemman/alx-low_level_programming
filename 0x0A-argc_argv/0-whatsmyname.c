5d1f1f6952a5d1c0166714b49a89844f08dfb720
Adedayo Adepoju <1mole.coco@gmail.com>
2022-04-01 18:09:48 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+
+		}
+	}
+	return (0);
+}
