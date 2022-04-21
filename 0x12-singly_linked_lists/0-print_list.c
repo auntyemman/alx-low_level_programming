c3ad7b88f72caf861ecd35e55b3a9843c7dc3d9e
Adedayo Adepoju <1mole.coco@gmail.com>
2022-04-21 23:08:27 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,17 +1,15 @@
 #include "lists.h"
-
-
 /**
  * print_list - prints all the elements of a list_t list
  * @h: head of linked list
  * Return: number of elements
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t size;
 
 	size = 0;
+
 	while (h != NULL)
 	{
 		size++;
